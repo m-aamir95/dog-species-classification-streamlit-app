6ca058ddc6c84a41b497677da0ea8e82028fbbb4
Aamir <djzaamir@gmail.com>
2023-11-11 17:32:17 +0000

    Added training_log.txt, also the inference is working, now need to apply Softmax and convert the logits back to the classnames

--- inference.py
+++ inference.py
@@ -1,11 +1,16 @@
 import gdown
-import torch
 import os
+
+import torch
+import torchvision.transforms as T
 
 from model.Dataset import CustomStanfordImageDataset
 from model.model import ConvolutionalNeuralNetwork
 
-def main():
+import cv2
+
+# Load model and put it do eval
+def load_model() -> ConvolutionalNeuralNetwork:
 
     model_url = "https://drive.google.com/file/d/1qaNErdLEUslRutwl0_H9-bBjPc-gHTK-/view"
     downloaded_model_name = "dogs_classification_cnn_model.pym"
@@ -19,8 +24,40 @@ def main():
     model = ConvolutionalNeuralNetwork().to(torch.device("cpu"))
     model.load_state_dict(torch.load(downloaded_model_name, map_location=torch.device("cpu")))
 
+    # Put the model to eval mode
+    model.eval()
 
-    
+    return model
+
+def make_prediction(input_image):
+
+    # Transform the image to required format
+    resize_width = 64
+    resize_height = 64
+
+    transforms = T.Compose([T.ToTensor(), 
+                            T.Grayscale(), 
+                            T.Resize((resize_width, resize_height), antialias=None),
+                            T.Normalize((0.5), (0.5))])
+    transformed_image = transforms(input_image)
+
+    with torch.no_grad():
+
+        #Reshape according to the expected shape of the model Batch x Channels x Width x Height
+        transformed_image.reshape(-1, 1, resize_width, resize_height)
+
+        model = load_model()
+
+        prediction = model(transformed_image)
+
+        print(prediction.shape)
+        print(prediction)
+
+
 
 if __name__ == "__main__":
-    main()
+
+    image_path = "Raw_Data/n02085620-Chihuahua/n02085620_199.jpg"
+    dog_image = cv2.imread(image_path)
+    
+    make_prediction(dog_image)
