16281fcbcecbf1126d8793ef1b4ca60e1a0330ed
Aamir <djzaamir@gmail.com>
2023-11-12 11:23:57 +0000

    Moved download_and_setup_dataset.sh to scripts directory

--- scripts/download_and_setup_dataset.sh
+++ scripts/download_and_setup_dataset.sh
@@ -4,11 +4,11 @@ if [ ! -d "./Images" ]
 then
   echo "Images dir not found, downloading dataset";
   #Download Dataset from stanford
-  wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar -O images.tar;
+  wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar -O ./images.tar;
   # Uncompressing tar ball
-  tar -xvf images.tar &> /dev/null;
+  tar -xvf ./images.tar &> /dev/null;
 
-  mkdir -p ./Raw_Data
+  mkdir ./Raw_Data
   mv ./Images ./Raw_Data/Images;
   rm ./images.tar
 fi
